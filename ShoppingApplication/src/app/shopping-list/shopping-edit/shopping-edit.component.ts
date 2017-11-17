f5b25256a88b1156755690d61d4af034998370f6
anitatripuraneni <anita.tripuraneni@gmail.com>
2017-11-17 14:56:39 +0530

    updated the shopping application

--- ShoppingApplication/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ShoppingApplication/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import {Component, ElementRef, OnInit, ViewEncapsulation, ViewChild, EventEmitter, Output} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
@@ -7,10 +7,17 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ShoppingEditComponent implements OnInit {
-
+ @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() IngredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onAddItem() {
+const ingName = this.nameInputRef.nativeElement.value;
+const ingAmount = this.amountInputRef.nativeElement.value;
+const newIngredient = new Ingredient(ingName, ingAmount);
+this.IngredientAdded.emit(newIngredient);
+  }
 }
