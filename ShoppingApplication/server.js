3e1662ae72139c3362ec1d07accf200cee8d9f8f
anitatripuraneni <anita.tripuraneni@gmail.com>
2017-12-11 09:23:41 +0530

    updated the shopping application with server config details

---
+++ ShoppingApplication/server.js
@@ -0,0 +1,26 @@
+'user strict';
+require('zone.js/dist/zone-node');
+require('reflect-metadata');
+
+const express = require('express');
+const ngUniversal = require('@nguniversal/express-engine');
+const{provideModuleMap} = require('@nguniversal/module-map-ngfactory-loader');
+const{AppServerModuleNgFactory,LAZY_MODULE_MAP}=require('./dist-server/main.b043310b467c246a361d.bundle');
+
+function angularRouter(req,res){
+    res.render('index',{req,res});
+}
+
+const app = express();
+app.engine('html',ngUniversal.ngExpressEngine({
+    bootstrap:AppServerModuleNgFactory,
+    providers:[
+        provideModuleMap(LAZY_MODULE_MAP)
+    ]
+}));
+app.set('view engine','html');
+app.set('views','dist');
+app.get('/',angularRouter);
+app.use(express.static('${_dirname}/dist'));
+app.get('*',angularRouter);
+app.listen('listening on port 3000');
--- ShoppingApplication/src/app/app.module.ts
+++ ShoppingApplication/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
 
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId:'my-unversial-app'}),
     HttpModule,
     HttpClientModule,
     AppRoutingModule,
